71281258cea8d5bbf03aeeec9aedbeedbc561dfb
JerryOlu-02 <joluwole20@gmail.com>
2026-06-23 13:05:15 +0100

    swapped canvas to napi-rs for cloud stability

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+
 const { Telegraf } = require("telegraf");
-const { createCanvas, loadImage } = require("canvas");
+const { createCanvas, loadImage } = require("@napi-rs/canvas");
+
 const axios = require("axios");
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
